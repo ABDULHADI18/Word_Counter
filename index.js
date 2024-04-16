4c14f40b542aacf9a95de9e386cb9365f1114264
ABDULHADI18 <160162625+ABDULHADI18@users.noreply.github.com>
2024-04-17 01:18:52 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 #! /usr/bin/env node
+import chalk from "chalk";
 import inquirer from "inquirer";
 const user_ans = await inquirer.prompt([
     {
         name: "Sentence",
         tyoe: "input",
-        message: "Enter your sentence to count the words : ",
-    }
+        message: chalk.blue("Enter your sentence to count the words : "),
+    },
 ]);
-const words = user_ans.Sentence.trim().split("");
-console.log(words);
-console.log(`Your sentence word count is ${words.length}`);
+const words = user_ans.Sentence.trim().split(" ");
+console.log(chalk.green(words));
+console.log(chalk.yellow(`Your sentence word count is ${words.length}`));
+console.log(chalk.red("THANK YOU FOR USING WORD COUNTER"));
--- index.ts
+++ index.ts
@@ -9,12 +9,15 @@ const user_ans: {
   {
     name: "Sentence",
     tyoe: "input",
-    message: "Enter your sentence to count the words : ",
+    message: chalk.blue("Enter your sentence to count the words : "),
   },
 ]);
 
-const words = user_ans.Sentence.trim().split("");
+const words = user_ans.Sentence.trim().split(" ");
 
-console.log(words);
+console.log(chalk.green(words));
 
-console.log(`Your sentence word count is ${words.length}`);
+console.log(chalk.yellow(`Your sentence word count is ${words.length}`));
+
+console.log(chalk.red("THANK YOU FOR USING WORD COUNTER"));
+
